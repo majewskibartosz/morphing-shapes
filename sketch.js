3dd5706ce548410b37f2ea61dfccedeba8daa91b
majewskibartosz <majewskibartosz@pm.me>
2023-05-22 01:23:30 +0200

    working bare bones morph shape app

--- sketch.js
+++ sketch.js
@@ -1,76 +1,70 @@
 import p5 from "p5";
 
-class Shape {
-  constructor(vertices) {
-    this.vertices = vertices;
-    this.targetVertices = [...vertices];
-  }
-
-  draw(p) {
-    p.stroke(255);
-    p.strokeWeight(2);
-    p.noFill();
-    p.beginShape();
-    this.vertices.forEach((v) => p.vertex(v.x, v.y));
-    p.endShape();
-  }
-
-  morph(targetVertices) {
-    this.targetVertices = targetVertices;
-  }
-
-  addLine(p, offsetX) {
-    if (this.vertices.length < 100) {
-      let angle = p.random(p.TWO_PI);
-      let x = p.width / 2 + 50 * p.cos(angle) + offsetX;
-      let y = p.height / 2 + 50 * p.sin(angle);
-      this.vertices.push(p.createVector(x, y));
-      this.targetVertices.push(p.createVector(x, y));
-    }
-  }
-
-  update(p) {
-    for (let i = 0; i < this.vertices.length; i++) {
-      let v = this.vertices[i];
-      let targetV = this.targetVertices[i % this.targetVertices.length];
-      this.vertices[i] = p5.Vector.lerp(v, targetV, 0.01);
-    }
-  }
-}
-
 new p5((p) => {
-  let shapes = [];
-  let offsetX = 0;
-  let lineCounter = 0;
+  let circle = [];
+  let square = [];
+  let morph = [];
+  let state = false;
 
   p.setup = function () {
-    let canvas = p.createCanvas(p.windowWidth, p.windowHeight);
-    canvas.position(0, 0);
-    canvas.style("z-index", "-1");
-    p.frameRate(30);
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(0);
 
-    let lineVertices = [
-      p.createVector(p.width / 2, p.height * 0.5),
-      p.createVector(p.width / 2, p.height),
-    ];
-    let line = new Shape(lineVertices);
-    shapes.push(line);
+    for (let angle = 0; angle < 360; angle += 9) {
+      let v = p5.Vector.fromAngle(p.radians(angle - 135));
+      v.mult(150); // Increase size by 50%
+      circle.push(v);
+      morph.push(p.createVector());
+    }
+
+    for (let x = -75; x < 75; x += 15) {
+      // Increase size by 50%
+      square.push(p.createVector(x, -75)); // Increase size by 50%
+    }
+    for (let y = -75; y < 75; y += 15) {
+      // Increase size by 50%
+      square.push(p.createVector(75, y)); // Increase size by 50%
+    }
+    for (let x = 75; x > -75; x -= 15) {
+      // Increase size by 50%
+      square.push(p.createVector(x, 75)); // Increase size by 50%
+    }
+    for (let y = 75; y > -75; y -= 15) {
+      // Increase size by 50%
+      square.push(p.createVector(-75, y)); // Increase size by 50%
+    }
   };
 
   p.draw = function () {
     p.background(0);
 
-    if (p.frameCount % 90 === 0 && lineCounter < 60) {
-      offsetX += 20;
-      lineCounter++;
-      shapes.forEach((shape) => shape.addLine(p, offsetX));
+    let totalDistance = 0;
+
+    for (let i = 0; i < circle.length; i++) {
+      let v1;
+      if (state) {
+        v1 = circle[i];
+      } else {
+        v1 = square[i];
+      }
+      let v2 = morph[i];
+      v2.lerp(v1, 0.1);
+      totalDistance += p5.Vector.dist(v1, v2);
     }
 
-    shapes.forEach((shape, i) => {
-      let targetShape = shapes[(i + 1) % shapes.length];
-      shape.morph(targetShape.vertices);
-      shape.update(p);
-      shape.draw(p);
+    if (totalDistance < 0.1) {
+      state = !state;
+    }
+
+    p.translate(p.width / 2, p.height / 2);
+    p.strokeWeight(4);
+    p.beginShape();
+    p.noFill();
+    p.stroke(255);
+
+    morph.forEach((v) => {
+      p.vertex(v.x, v.y);
     });
+    p.endShape(p.CLOSE);
   };
 });
